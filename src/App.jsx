f78ccda8051c6c1a932c904d2b0f943bbbac89bb
Lavanya Joshi <lavanyajoshi1124@gmail.com>
2025-12-16 09:55:36 +0530

    Redid Counter App and Added Final Touches

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="container">
-      <h1>Count Check</h1>
-      <p>Count: {count}</p>
-
+      <h1>Counter App</h1>
+      <p>Current Count: {count}</p>
       <div className="buttons">
-        <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
-        <button onClick={() => setCount((prev) => prev - 1)}>Decrease</button>
+        <button onClick={() => setCount((prev) => prev + 1)}>
+          Increase Count
+        </button>
+        <button onClick={() => setCount((prev) => prev - 1)}>
+          Decrease Count
+        </button>
         <button onClick={() => setCount(0)}>Reset</button>
       </div>
     </div>
   );
 }
+
 export default App;
